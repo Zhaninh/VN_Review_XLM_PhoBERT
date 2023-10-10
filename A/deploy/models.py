7f45a0e92d39850ec03041b23db3cc52c6d0eed9
Aaron <119613369+Zhaninh@users.noreply.github.com>
2023-10-10 16:10:38 +0700

    Update models.py

--- A/deploy/models.py
+++ A/deploy/models.py
@@ -78,7 +78,7 @@ class CustomXLMModel(nn.Module):
 
 class CustomXLMModel_v2(nn.Module):
     def __init__(self, num_classification_labels=6, num_regression_neurons=30):
-        super(CustomXLMModel, self).__init__()
+        super(CustomXLMModel_v2, self).__init__()
         # Load a pre-trained XLM model
         self.model = AutoModel.from_pretrained("xlm-roberta-base", output_attentions=True,output_hidden_states=True)
         
