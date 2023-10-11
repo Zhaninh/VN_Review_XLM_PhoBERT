9a0bb6d36b7fbd4793f43b557dc87156b947527f
Aaron <119613369+Zhaninh@users.noreply.github.com>
2023-10-11 14:51:15 +0700

    Update models.py

--- A/deploy/models.py
+++ A/deploy/models.py
@@ -119,52 +119,25 @@ class CustomXLMModel_v2(nn.Module):
 
 class CustomBERTModel(nn.Module):
     def __init__(self, num_classification_labels=6, num_regression_neurons=30):
-        super(CustomXLMModel_v2, self).__init__()
+        super(CustomXLMModel, self).__init__()
         # Load a pre-trained XLM model
         self.model = AutoModel.from_pretrained("vinai/phobert-base", output_attentions=True,output_hidden_states=True)
         
-        # Layer 1
-        self.dropout1 = nn.Dropout(0.1)
-        self.layer1 = nn.Linear(self.model.config.hidden_size * 4, 512)
-        self.bn1 = nn.BatchNorm1d(512)
-        self.reLu = nn.ReLU()
-        
-        # Layer Classification
-        self.dropout2 = nn.Dropout(0.1)
+        # Define layers
+        self.dropout = nn.Dropout(0.1)
         self.classifier = nn.Linear(self.model.config.hidden_size * 4, num_classification_labels)
-        self.bn2 = nn.BatchNorm1d(num_classification_labels)
-        
-        # Layer Regression
         self.regressor = nn.Linear(self.model.config.hidden_size * 4, num_regression_neurons)
 
-        nn.init.xavier_uniform_(self.layer1)
-        nn.init.xavier_uniform_(self.classifier)
-        nn.init.xavier_uniform_(self.regressor)
-
-    
     def forward(self, input_ids, attention_mask):
         # Forward pass through XLM model
         outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
         outputs = torch.cat((outputs.hidden_states[-1][:, 0, ...], outputs.hidden_states[-2][:, 0, ...], outputs.hidden_states[-3][:, 0, ...], outputs.hidden_states[-4][:, 0, ...]), -1)
-            
-        # Apply layer 1
-        outputs = self.dropout1(outputs)
-        outputs = self.layer1(outputs)
-        outputs = self.bn1(outputs)
-        outputs = F.relu(outputs)
-        
-        # Apply classification layer
-        outputs_classifier = self.dropout2(outputs_classifier)
+        outputs = self.dropout(outputs)
         outputs_classifier = self.classifier(outputs)
-        outputs_classifier = self.bn2(outputs_classifier)
-        outputs_classifier = torch.sigmoid(outputs_classifier)
-        
-        # Apply regression layer
         outputs_regressor = self.regressor(outputs)
+        outputs_classifier = torch.sigmoid(outputs_classifier)
         outputs_regressor = outputs_regressor.view(-1, 6, 5)
-        
         return outputs_classifier, outputs_regressor
-
 
 
 
