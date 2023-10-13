30c05c40c3203e917d2211d7cd311e7760f5692a
Aaron <119613369+Zhaninh@users.noreply.github.com>
2023-10-13 19:15:13 +0700

    Update models.py

--- A/deploy/models.py
+++ A/deploy/models.py
@@ -120,7 +120,7 @@ class CustomXLMModel_v2(nn.Module):
 class CustomBERTModel(nn.Module):
     def __init__(self, num_classification_labels=6, num_regression_neurons=30):
         super(CustomBERTModel, self).__init__()
-        # Load a pre-trained XLM model
+        # Load a pre-trained BERT model
         self.model = AutoModel.from_pretrained("vinai/phobert-base", output_attentions=True,output_hidden_states=True)
         
         # Define layers
